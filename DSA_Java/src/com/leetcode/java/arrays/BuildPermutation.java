69742ae86e8a2107406b78bae6eb901e5abcb1a4
Mohit Gupta <Mohit.Gupta14@ibm.com>
2024-07-26 00:06:24 +0530

    Build Permutation Class Added for Java

---
+++ DSA_Java/src/com/leetcode/java/arrays/BuildPermutation.java
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/build-array-from-permutation/
+
+package com.leetcode.java.arrays;
+
+public class BuildPermutation {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] buildArray(int[] nums) {
+        int[] res = new int[5] ;
+        return res;
+    }
+}
